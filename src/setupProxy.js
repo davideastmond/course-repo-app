6a5dbb398f1a373572a4996f3fd145b8603164fc
David Eastmond <davebuck1981@gmail.com>
2021-08-30 18:38:22 -0400

    modified proxy for production use

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,16 @@
+const isProduction = !(
+  process.env.NODE_ENV && process.env.NODE_ENV.match("development")
+);
+const API_URL = isProduction
+  ? process.env.REACT_APP_PRODUCTION_API_URL
+  : process.env.REACT_APP_API_URL;
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: API_URL,
       changeOrigin: true,
     })
   );

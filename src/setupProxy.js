e2b3c13a9280e2b653dd83bd3aafc54939ebae56
David Eastmond <davebuck1981@gmail.com>
2021-08-31 15:35:04 -0400

    clean up proxy stuff

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -8,20 +8,17 @@ const API_URL = isProduction
   : process.env.REACT_APP_API_URL;
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-console.log("APIURL is", API_URL);
-console.log("NODE_ENV", process.env.NODE_ENV);
-console.log("PRODUCTION API", process.env.REACT_APP_PRODUCTION_API_URL);
 module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: process.env.REACT_APP_PRODUCTION_API_URL,
+      target: API_URL,
       changeOrigin: true,
     })
   );
-  // app.use(express.static(path.join(__dirname), "/public"));
-  // app.use(express.static(path.join(__dirname, "/build")));
-  // app.get("*", (req, res, next) => {
-  //   res.sendFile(path.join(__dirname + "/build/index.html"));
-  // });
+
+  app.use(express.static(path.join(__dirname, "/build")));
+  app.get("*", (req, res, next) => {
+    res.sendFile(path.join(__dirname + "/build/index.html"));
+  });
 };

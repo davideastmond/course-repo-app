52b2cf533379620a0c04f0c071d2b316afc1caa1
David Eastmond <davebuck1981@gmail.com>
2021-09-02 08:06:52 -0400

    test

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,26 +1,26 @@
-// const express = require("express");
-// const path = require("path");
-// const isProduction = !(
-//   process.env.NODE_ENV && process.env.NODE_ENV.match("development")
-// );
-// const API_URL = isProduction
-//   ? process.env.REACT_APP_PRODUCTION_API_URL
-//   : process.env.REACT_APP_API_URL;
-// const { createProxyMiddleware } = require("http-proxy-middleware");
+const express = require("express");
+const path = require("path");
+const isProduction = !(
+  process.env.NODE_ENV && process.env.NODE_ENV.match("development")
+);
+const API_URL = isProduction
+  ? process.env.REACT_APP_PRODUCTION_API_URL
+  : process.env.REACT_APP_API_URL;
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-// console.log("API URL", API_URL);
-// module.exports = function (app) {
-//   app.use(
-//     "/api",
-//     createProxyMiddleware({
-//       target: API_URL,
-//       changeOrigin: true,
-//     })
-//   );
-//   if (isProduction) {
-//     app.use(express.static(path.join(__dirname, "/build")));
-//     app.get("*", (req, res, next) => {
-//       res.sendFile(path.join(__dirname + "/build/index.html"));
-//     });
-//   }
-// };
+console.log("API URL", API_URL);
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: API_URL,
+      changeOrigin: true,
+    })
+  );
+  if (isProduction) {
+    app.use(express.static(path.join(__dirname, "/build")));
+    app.get("*", (req, res, next) => {
+      res.sendFile(path.join(__dirname + "/build/index.html"));
+    });
+  }
+};
